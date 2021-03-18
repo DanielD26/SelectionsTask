9582cf14c72d384f7d90cdd9d9c3da9fef926524
DanielD26 <103547416@student.swin.edu.au>
2021-03-18 16:02:38 +1100

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Selections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age: ");
+            string age = Console.ReadLine();
+
+            int ageNum = int.Parse(age);
+
+            if (ageNum > 17) {
+                Console.WriteLine("Legally adult age: ");          
+            } else {
+                Console.WriteLine("Legally not adult age");
+            }
+        }
+    }
+}
